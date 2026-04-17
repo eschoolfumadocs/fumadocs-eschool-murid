b783c527f58829553560cfb1ff9be1b36cdb574c
Fajarihsan <7afajarihsan@gmail.com>
2026-04-17 02:22:32 +0000

    Edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,9 @@ module.exports = {
             name: "Fumadocs Murid",
             script: "node_modules/next/dist/bin/next",
             args: "start -p 3002",
-            cwd: "./",
-            exec_mode: "cluster",
-            instances: "max",
+            cwd: "/www/wwwroot/fumadocs-eschool-murid", // ⬅️ WAJIB ABSOLUTE
+            exec_mode: "fork",           // ⬅️ GANTI INI
+            instances: 1,                // ⬅️ SIMPLE DULU
             autorestart: true,
             watch: false,
             max_memory_restart: "3G",
